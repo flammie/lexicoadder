a98af245f756b68947be93597dd8455648c97f93
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2020-01-27 18:51:46 +0100

    app

--- lexicoadder.py
+++ lexicoadder.py
@@ -1,37 +1,115 @@
 #!/usr/bin/env -O python3
 """First prototype for an dictionary extension workflow app."""
 
+from sys import argv
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import GLib, Gio, Gtk
 
+# This would typically be its own file
+MENU_XML="""
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <menu id="app-menu">
+    <section>
+      <item>
+        <attribute name="action">app.open</attribute>
+        <attribute name="label" translatable="yes">_Open...</attribute>
+      </item>
+      <item>
+        <attribute name="action">app.about</attribute>
+        <attribute name="label" translatable="yes">_About</attribute>
+      </item>
+      <item>
+        <attribute name="action">app.quit</attribute>
+        <attribute name="label" translatable="yes">_Quit</attribute>
+        <attribute name="accel">&lt;Primary&gt;q</attribute>
+    </item>
+    </section>
+  </menu>
+</interface>
+"""
 
-class LexicoAdderWindow(Gtk.Window):
-    """Main window of the app."""
+class LexicoAdderWindow(Gtk.ApplicationWindow):
 
-    def __init__(self):
-        """Initialise GTK GUI."""
-        Gtk.Window.__init__(self, title="Hello World")
-
-        headboard = Gtk.HeaderBar()
-        headboard.set_show_close_button(True)
-        headboard.props.title = "Add words to lexicons!!"
-        self.set_titlebar(headboard)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.box = Gtk.Box(spacing=6)
         self.add(self.box)
-
+        self.box.show()
         self.texti = Gtk.TextView()
         self.box.pack_start(self.texti, True, True, 0)
+        self.texti.show()
 
 
-def main():
-    """Parse args, create GUI."""
-    win = LexicoAdderWindow()
-    win.connect("destroy", Gtk.main_quit)
-    win.show_all()
-    Gtk.main()
 
+class LexicoAdderApplication(Gtk.Application):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,
+                         application_id="com.flammie.github.lexicoadder",
+                         flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
+                         **kwargs)
+        self.window = None
+        self.add_main_option("test", ord("t"), GLib.OptionFlags.NONE,
+                             GLib.OptionArg.NONE, "Command line test", None)
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+        action = Gio.SimpleAction.new("about", None)
+        action.connect("activate", self.on_about)
+        self.add_action(action)
+        action = Gio.SimpleAction.new("quit", None)
+        action.connect("activate", self.on_quit)
+        self.add_action(action)
+        action = Gio.SimpleAction.new("open", None)
+        action.connect("activate", self.on_open)
+        self.add_action(action)
+        builder = Gtk.Builder.new_from_string(MENU_XML, -1)
+        self.set_app_menu(builder.get_object("app-menu"))
+
+    def do_activate(self):
+        # We only allow a single window and raise any existing ones
+        if not self.window:
+            # Windows are associated with the application
+            # when the last one is closed the application shuts down
+            self.window = LexicoAdderWindow(application=self,
+                                            title="Lexicoadder")
+
+        self.window.present()
+
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        # convert GVariantDict -> GVariant -> dict
+        options = options.end().unpack()
+        if "test" in options:
+            # This is printed on the main instance
+            print("Test argument recieved: %s" % options["test"])
+        self.activate()
+        return 0
+
+    def on_about(self, action, param):
+        about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
+        about_dialog.present()
+
+    def on_quit(self, action, param):
+        self.quit()
+
+    def on_open(self, action, param):
+        dialog = Gtk.FileChooserDialog("Please choose a file", self.window,
+                                       Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_CANCEL,
+                                        Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+        dialog.destroy()
 
 if __name__ == "__main__":
-    main()
+    app = LexicoAdderApplication()
+    app.run(argv)
